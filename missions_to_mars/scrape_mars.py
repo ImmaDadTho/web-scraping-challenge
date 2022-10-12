c88f8d07da328ea85b4b8984d43fc6aafe762345
Hector Custodio <custodio.hector@live.com>
2022-10-12 19:18:33 -0400

    Update scrape_mars.py

--- missions_to_mars/scrape_mars.py
+++ missions_to_mars/scrape_mars.py
@@ -3,19 +3,43 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as soup
 import requests
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-s = Service('C:/Users/.../chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+# from selenium.webdriver.chrome.service import Service
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# s = Service('C:/Users/.../chromedriver.exe')
+# driver = webdriver.Chrome(service=s)
 
 # scrape all function
 def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+
+    scrape_news(browser)
+
     #print("Scrape Scrape")
-    browser.quit()
+    #browser.quit()
 
 # scrape the mars news page 
+def scrape_news(browser):
+    # syntax used to visit the mars site 
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
+
+    #creates a delay to properly get data from scraping 
+    browser.is_element_present_by_css('div.list_text', wait_time=1.5)
+
+    souphtml = browser.html
+    marsnewssoup = soup(souphtml, 'html.parser')
+
+    sections = marsnewssoup.select_one('div.list_text')
+
+    news_title = sections.find('div', class_='content_title').get_text()
+
+    news_p = sections.find('div', class_="article_teaser_body").get_text()
+
+    return news_title, news_p
+
+
+
 
 # scrape the featured image page 
 
