64764f9cac61ead0e22402fa057509872ab9de30
Hector Custodio <custodio.hector@live.com>
2022-10-12 20:43:50 -0400

    Update app.py

--- missions_to_mars/app.py
+++ missions_to_mars/app.py
@@ -26,7 +26,7 @@ def scrape():
     #loads the dictionary into mongo DB
     marsGoDb.insert_one(mars_data)
 
-    return (f"You have reached the planet Mars\n" %{mars_data})
+    return "You have reached the planet Mars\n", mars_data
 
 if __name__ == "__main__":
     app.run()
